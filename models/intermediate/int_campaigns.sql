87efb1a92e29a4388d97fac44808b8d680ebb90d
Jette Bremm <bremmjette5@gmail.com>
2024-05-08 21:22:34 +0000

    -

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -11,8 +11,8 @@ WITH union_all_data AS (
     SELECT * 
     FROM {{ref("stg_raw__facebook")}}
 )
-SELECT campaign_key
+SELECT campaign_name
      , COUNT(*) AS row_count
 FROM union_all_data
-GROUP BY campaign_key
+GROUP BY campaign_name
 HAVING COUNT(*) > 2
