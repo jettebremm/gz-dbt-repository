2069f2df3bd4c635ef68c368dd208428f09d994a
Jette Bremm <bremmjette5@gmail.com>
2024-05-08 20:09:00 +0000

    created subquery to test campaign_key for primary key

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,11 +1,18 @@
-SELECT *
-FROM {{ref("stg_raw__adwords")}}
-UNION ALL
-SELECT *
-FROM {{ref("stg_raw__bing")}}
-UNION ALL
-SELECT *
-FROM {{ref("stg_raw__criteo")}}
-UNION ALL
-SELECT * 
-FROM {{ref("stg_raw__facebook")}}
+WITH union_all_data AS (
+    SELECT *
+    FROM {{ref("stg_raw__adwords")}}
+    UNION ALL
+    SELECT *
+    FROM {{ref("stg_raw__bing")}}
+    UNION ALL
+    SELECT *
+    FROM {{ref("stg_raw__criteo")}}
+    UNION ALL
+    SELECT * 
+    FROM {{ref("stg_raw__facebook")}}
+)
+SELECT campaign_key
+     , COUNT(*) AS row_count
+FROM union_all_data
+GROUP BY campaign_key
+HAVING COUNT(*) > 2
