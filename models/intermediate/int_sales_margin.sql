b09443aa80814b437229f7943daa3acd7bc5e897
Jette Bremm <bremmjette5@gmail.com>
2024-05-08 14:41:30 +0000

    -

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,7 +3,10 @@ SELECT
     date_date, 
     orders_id,
     revenue, 
-    quantity
-FROM {{ref("stg_raw__sales")}} s
-LEFT JOIN {{ref("stg_raw__product")}} p 
+    quantity,
+    purchase_price, 
+    ROUND(quantity*purchase_price,2) AS purchase_cost,
+    revenue - (quantity*purchase_price) AS margin
+FROM {{ref("stg_raw__sales")}}
+LEFT JOIN {{ref("stg_raw__product")}}
     USING (products_id)
