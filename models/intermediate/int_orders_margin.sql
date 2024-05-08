4cf12bbe36993edc3944bdcaa6d1ac44004189a1
Jette Bremm <bremmjette5@gmail.com>
2024-05-08 14:22:07 +0000

    -

--- models/intermediate/int_orders_margin.sql
+++
@@ -1,8 +0,0 @@
-SELECT orders_id
-, date_date
-, revenue
-, margin
-, quantity
-, purchase_cost
-FROM {{ref("int_sales_margin")}}
-ORDER BY orders_id DESC
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,10 +1,9 @@
-SELECT
-  orders_id,
-  max(date_date) as date_date,
-  ROUND(SUM(revenue),2) as revenue,
-  ROUND(SUM(quantity),2) as quantity,
-  ROUND(SUM(purchase_cost),2) as purchase_cost,
-  ROUND(SUM(margin),2) as margin
-FROM {{ ref("int_sales_margin") }}
-GROUP BY orders_id
-ORDER BY orders_id DESC
+SELECT 
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+    USING (products_id)
